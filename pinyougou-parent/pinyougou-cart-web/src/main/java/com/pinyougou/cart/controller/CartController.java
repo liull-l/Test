dc01900d446a1b5ada85cbaeec5dbae6bf25c84d
liull-l <1263672225@qq.com>
2019-06-16 23:39:03 +0800

    合算

---
+++ pinyougou-parent/pinyougou-cart-web/src/main/java/com/pinyougou/cart/controller/CartController.java
@@ -0,0 +1,106 @@
+package com.pinyougou.cart.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.fastjson.JSON;
+import com.pinyougou.cart.service.CartService;
+import com.pinyougou.pojogroup.Cart;
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import entity.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cart")
+public class CartController {
+    @Reference(timeout = 6000)
+    private CartService cartService;
+    @Autowired
+    private HttpServletRequest request;
+    @Autowired
+    private HttpServletResponse response;
+
+    /**
+     * 购物车列表111111
+     * @param
+     * @return
+     */
+
+    @RequestMapping("/findCartList")
+    public List<Cart> findCartList(){
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        String cartListString=util.CookieUtil.getCookieValue(request,"cartList","UTF-8");
+        System.out.println("当前登录人--------"+username);
+        if (cartListString==null || cartListString.equals("")){
+            cartListString="[]";
+        }
+        List<Cart> cartList_cookie= JSON.parseArray(cartListString,Cart.class);
+        if (username.equals("anonymousUser")){//如果未登录
+            //从cookie中提取购物车
+            System.out.println("从cookie中提取购物车");
+            return cartList_cookie;
+        }else {//如果已登录
+            //获取redis购物车
+            List<Cart> cartList_redis=cartService.findCartListFromRedis(username);
+            System.out.println("从redis中提取购物车");
+            if (cartList_cookie.size()>0){//如果本地存在购物车
+                //合并购物车
+                cartList_redis=cartService.mergeCartList(cartList_redis,cartList_cookie);
+                //清除本地cookie的数据
+                util.CookieUtil.deleteCookie(request,response,"cartList");
+                //将合并后的数据存入到redis
+                cartService.saveCartListToRedis(username,cartList_redis);
+            }
+            return cartList_redis;
+
+        }
+
+    }
+    /**
+     * 添加商品到购物车
+     * @param
+     * @param
+     * @param itemId
+     * @param num
+     * @return
+     */
+
+    @RequestMapping("/addGoodsToCartList")
+    @CrossOrigin(origins="http://localhost:9105",allowCredentials="true")
+    public Result addGoodsToCartList(Long itemId, Integer num) {
+
+       // response.setHeader("Access-Control-Allow-Origin", "http://localhost:9105");
+        //response.setHeader("Access-Control-Allow-Credentials", "true");
+
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        System.out.println("当前登录用户：" + username);
+        try {
+            List<Cart> cartList = findCartList();//获取购物车列表
+            cartList = cartService.addGoodsToCartList(cartList, itemId, num);
+            if (username.equals("anonymouserUser")) {//如果是未登录，保存到cookie
+                util.CookieUtil.setCookie(request, response, "cartList", JSON.toJSONString(cartList), 3600 * 24, "UTF-8");
+                System.out.println("向cookie存入到数据");
+            } else {//如果是已登录，保存到redis
+                cartService.saveCartListToRedis(username, cartList);
+            }
+            return new Result(true, "添加成功");
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            return new Result(false, e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "添加失败");
+
+        }
+    }
+
+
+
+}
---
+++ pinyougou-parent/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
@@ -0,0 +1,50 @@
+//购物车控制层
+app.controller('cartController',function($scope,cartService){
+	//查询购物车列表
+	$scope.findCartList=function(){
+		cartService.findCartList().success(
+			function(response){
+				$scope.cartList=response;
+				$scope.totalValue= cartService.sum($scope.cartList);
+			}
+		);
+	}
+	
+	//数量加减
+	$scope.addGoodsToCartList=function(itemId,num){
+		cartService.addGoodsToCartList(itemId,num).success(
+			function(response){
+				if(response.success){//如果成功
+					$scope.findCartList();//刷新列表
+				}else{
+					alert(response.message);
+				}				
+			}		
+		);		
+	}
+
+    //获取地址列表
+    $scope.findAddressList=function(){
+
+		cartService.findAddressList().success(
+			function (response) {
+				$scope.addressList=response;
+
+            }
+		)
+    }
+    //选择地址
+	$scope.selectAddress=function (address) {
+		$scope.address=address;
+
+    }
+    //判断是否是当前选中的地址
+    $scope.isSelectedAddress=function(address){
+        if(address==$scope.address){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+});
