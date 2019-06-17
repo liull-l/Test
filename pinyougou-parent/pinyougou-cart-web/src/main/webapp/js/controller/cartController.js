c69432496b413f97a02f4cbf44a12f5c2b460dc5
liull-l <1263672225@qq.com>
2019-06-17 21:01:33 +0800

    合算

--- pinyougou-parent/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
+++ pinyougou-parent/pinyougou-cart-web/src/main/webapp/js/controller/cartController.js
@@ -29,6 +29,14 @@ app.controller('cartController',function($scope,cartService){
 		cartService.findAddressList().success(
 			function (response) {
 				$scope.addressList=response;
+				//设置默认地址
+				for(var i=0;i<$scope.addressList.length;i++){
+					if($scope.addressList[i].isDefault=='1'){
+						$scope.address=$scope.addressList[i];
+						break;
+
+					}
+				}
 
             }
 		)
@@ -45,6 +53,37 @@ app.controller('cartController',function($scope,cartService){
         }else{
             return false;
         }
+    }
+
+    $scope.order={paymentType:'1'};
+	//选择支付方式
+	$scope.selectPayType=function (type) {
+		$scope.order.paymentType=type;
+
+    }
+    
+    //保存订单
+	$scope.submitOrder=function () {
+		$scope.order.receiverAreaName=$scope.address.address;//地址
+		$scope.order.receiverMobile=$scope.address.mobile;//手机
+		$scope.order.receiverAreaName = $scope.address.contact;//联系人
+		cartService.submitOrder($scope.order).success(
+			function (response) {
+				if(response.success()){
+					//页面跳转
+					if($scope.order.paymentType=='1'){//如果是微信支付，跳转到支付页面qwer
+						location.href='pay.html';
+
+					}else{//如果货到付款，跳转到提示页面
+						location.href="paysuccess.html";
+					}else{
+						alert(response.message);//也可以跳转到提示页面
+					}
+
+				}
+
+            }
+		)
     }
 
 });
